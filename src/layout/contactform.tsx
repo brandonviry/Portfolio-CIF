9f09b84d3e8dd3460c31452186d2d36c50c8c131
brandonviry <brandonviry@gmail.com>
2024-03-21 10:41:55 +0400

    upd: formulaire contact

--- src/layout/contactform.tsx
+++ src/layout/contactform.tsx
@@ -1,23 +1,16 @@
-import { Card } from '@/components/ui/card'
-import React from 'react'
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card";
+import React from "react";
 
 
 export default function contactform() {
-  const flag = false
-  return (
-     (flag)? 
-     
-     <Card style={{padding:"3%" , margin:"3%"}} className='flex flex-col'>
-      <h1 className='w-scree text-center text-2xl'>Contact</h1>
-     <Input type="email" placeholder="Email"  style={{margin:"1%"}}/>
-     <Input type="Text" placeholder="Object" style={{margin:"1%"}}/>
-
-     <Textarea placeholder="Contenue..." style={{margin:"1%"}}/>
-     <Button style={{margin:"1%"}} className='  bg-sky-900'>envoyer</Button>
-
- </Card>: ""
-  )
+  const flag = true;
+  return flag ? (
+    <Card style={{ padding: "3%", margin: "3%",  height:"80vh"}} className="flex flex-col">
+      <iframe style={{height:"100%" , width:"100%"}} src="https://docs.google.com/forms/d/e/1FAIpQLScMXYPUGQ7MoT9kRKTy3DvH4-XRM_ULQUmEnkMfYcbNF1JdkA/viewform?embedded=true">
+        Chargement…
+      </iframe>
+    </Card>
+  ) : (
+    ""
+  );
 }
